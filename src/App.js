54c1a245772eed4aaa9e299744cc19908040d465
“Berenice <berenice.herrera2011@gmail.com>
2024-02-23 22:32:47 -0300

    to do list app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import Header from './components/Header/Header';
+import Input from './components/Input/input'
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Input />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+const Header=()=>{
+    return (
+        <header className="appHeader">
+        <h1 className="appTitle">My ToDo List 📃</h1>
+      </header>
+    )
+}
+
+export default Header
